4b577ca12e13073351c2a63e7d084d061b36afb6
Gaziz <maks.mafia00@mail.ru>
2023-04-08 01:53:50 +0600

    refactor(app/providers): change suspence fallback ui

--- src/app/providers/with_router.tsx
+++ src/app/providers/with_router.tsx
@@ -1,10 +1,11 @@
+import { Spin } from 'antd'
 import { type ReactNode, Suspense } from 'react'
 import { BrowserRouter } from 'react-router-dom'
 
 export function withRouter(component: () => ReactNode) {
 	return () => (
 		<BrowserRouter>
-			<Suspense fallback={<div className='overload' />}>{component()}</Suspense>
+			<Suspense fallback={<Spin className='overlay' />}>{component()}</Suspense>
 		</BrowserRouter>
 	)
 }
