31c462cee904a2e08de7b98a64df50dff1b9b02c
Gaziz <maks.mafia00@mail.ru>
2023-04-08 01:30:09 +0600

    change(app): app instance

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,5 +1,10 @@
+import '~src/app/providers/index'
+import { withProviders } from '~src/app/providers/index'
+
+import './styles.scss'
+
 function App() {
 	return <>SelfMent</>
 }
 
-export default App
+export default withProviders(App)
