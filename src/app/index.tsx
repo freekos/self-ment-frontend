95c09b2008f784667a4da314e0aae25fdec7c0f0
Gaziz <maks.mafia00@mail.ru>
2023-04-08 20:00:20 +0600

    refactor(app/index.tsx): add pages

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,10 +1,12 @@
 import '~src/app/providers/index'
 import { withProviders } from '~src/app/providers/index'
 
+import { Pages } from '~src/pages'
+
 import './styles.scss'
 
 function App() {
-	return <>SelfMent</>
+	return <Pages />
 }
 
 export default withProviders(App)
